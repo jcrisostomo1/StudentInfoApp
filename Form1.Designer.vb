b3bf999ee22aa260dd4aae516c01f074b922e3a1
jcrisostomo1 <joshuacr22@gmail.com>
2022-09-14 15:17:02 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BaseForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(BaseForm))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.txtTitle = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.lblPostCode = New System.Windows.Forms.Label()
+        Me.txtPostCode = New System.Windows.Forms.TextBox()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnAddNew = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel5.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(28, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1536, 123)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Controls.Add(Me.txtTitle)
+        Me.Panel2.Location = New System.Drawing.Point(20, 17)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1497, 88)
+        Me.Panel2.TabIndex = 1
+        '
+        'txtTitle
+        '
+        Me.txtTitle.AutoSize = True
+        Me.txtTitle.BackColor = System.Drawing.SystemColors.Control
+        Me.txtTitle.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtTitle.ForeColor = System.Drawing.Color.CadetBlue
+        Me.txtTitle.Location = New System.Drawing.Point(421, 0)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(654, 86)
+        Me.txtTitle.TabIndex = 2
+        Me.txtTitle.Text = "Student Information"
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.CadetBlue
+        Me.Panel3.Controls.Add(Me.Panel4)
+        Me.Panel3.Location = New System.Drawing.Point(28, 141)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1041, 788)
+        Me.Panel3.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Panel4.Controls.Add(Me.lblPhone)
+        Me.Panel4.Controls.Add(Me.txtPhone)
+        Me.Panel4.Controls.Add(Me.lblPostCode)
+        Me.Panel4.Controls.Add(Me.txtPostCode)
+        Me.Panel4.Controls.Add(Me.lblAddress)
+        Me.Panel4.Controls.Add(Me.txtAddress)
+        Me.Panel4.Controls.Add(Me.lblLastName)
+        Me.Panel4.Controls.Add(Me.txtLastName)
+        Me.Panel4.Controls.Add(Me.lblFirstName)
+        Me.Panel4.Controls.Add(Me.txtFirstName)
+        Me.Panel4.Controls.Add(Me.DataGridView1)
+        Me.Panel4.Location = New System.Drawing.Point(26, 17)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(989, 755)
+        Me.Panel4.TabIndex = 1
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Font = New System.Drawing.Font("Segoe UI", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblPhone.Location = New System.Drawing.Point(18, 366)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(188, 71)
+        Me.lblPhone.TabIndex = 20
+        Me.lblPhone.Text = "Phone"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(294, 366)
+        Me.txtPhone.Multiline = True
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(651, 71)
+        Me.txtPhone.TabIndex = 19
+        '
+        'lblPostCode
+        '
+        Me.lblPostCode.AutoSize = True
+        Me.lblPostCode.Font = New System.Drawing.Font("Segoe UI", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblPostCode.Location = New System.Drawing.Point(11, 276)
+        Me.lblPostCode.Name = "lblPostCode"
+        Me.lblPostCode.Size = New System.Drawing.Size(279, 71)
+        Me.lblPostCode.TabIndex = 18
+        Me.lblPostCode.Text = "Post Code"
+        '
+        'txtPostCode
+        '
+        Me.txtPostCode.Location = New System.Drawing.Point(294, 276)
+        Me.txtPostCode.Multiline = True
+        Me.txtPostCode.Name = "txtPostCode"
+        Me.txtPostCode.Size = New System.Drawing.Size(651, 71)
+        Me.txtPostCode.TabIndex = 17
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Font = New System.Drawing.Font("Segoe UI", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblAddress.Location = New System.Drawing.Point(18, 190)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(229, 71)
+        Me.lblAddress.TabIndex = 16
+        Me.lblAddress.Text = "Address"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(294, 186)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(651, 71)
+        Me.txtAddress.TabIndex = 15
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Font = New System.Drawing.Font("Segoe UI", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblLastName.Location = New System.Drawing.Point(11, 106)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(269, 71)
+        Me.lblLastName.TabIndex = 14
+        Me.lblLastName.Text = "Lastname"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(294, 106)
+        Me.txtLastName.Multiline = True
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(651, 71)
+        Me.txtLastName.TabIndex = 13
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Font = New System.Drawing.Font("Segoe UI", 19.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblFirstName.Location = New System.Drawing.Point(11, 18)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(277, 71)
+        Me.lblFirstName.TabIndex = 12
+        Me.lblFirstName.Text = "Firstname"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(294, 29)
+        Me.txtFirstName.Multiline = True
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(651, 71)
+        Me.txtFirstName.TabIndex = 11
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(18, 453)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 82
+        Me.DataGridView1.RowTemplate.Height = 41
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(940, 290)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.CadetBlue
+        Me.Panel5.Controls.Add(Me.Panel6)
+        Me.Panel5.Location = New System.Drawing.Point(1090, 141)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(474, 788)
+        Me.Panel5.TabIndex = 2
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel6.Controls.Add(Me.btnExit)
+        Me.Panel6.Controls.Add(Me.btnReset)
+        Me.Panel6.Controls.Add(Me.btnDelete)
+        Me.Panel6.Controls.Add(Me.btnUpdate)
+        Me.Panel6.Controls.Add(Me.btnAddNew)
+        Me.Panel6.Location = New System.Drawing.Point(13, 17)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(442, 755)
+        Me.Panel6.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.Location = New System.Drawing.Point(57, 470)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(329, 84)
+        Me.btnExit.TabIndex = 27
+        Me.btnExit.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.btnExit, "Exit")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnReset.Location = New System.Drawing.Point(57, 380)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(329, 84)
+        Me.btnReset.TabIndex = 25
+        Me.btnReset.Text = "Reset"
+        Me.ToolTip1.SetToolTip(Me.btnReset, "Reset")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDelete.Location = New System.Drawing.Point(57, 290)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(329, 84)
+        Me.btnDelete.TabIndex = 24
+        Me.btnDelete.Text = "Delete"
+        Me.ToolTip1.SetToolTip(Me.btnDelete, "Delete")
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnUpdate.Location = New System.Drawing.Point(57, 200)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(329, 84)
+        Me.btnUpdate.TabIndex = 23
+        Me.btnUpdate.Text = "Update"
+        Me.ToolTip1.SetToolTip(Me.btnUpdate, "Update")
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnAddNew
+        '
+        Me.btnAddNew.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnAddNew.Location = New System.Drawing.Point(57, 110)
+        Me.btnAddNew.Name = "btnAddNew"
+        Me.btnAddNew.Size = New System.Drawing.Size(329, 84)
+        Me.btnAddNew.TabIndex = 22
+        Me.btnAddNew.Text = "Add New"
+        Me.ToolTip1.SetToolTip(Me.btnAddNew, "Add New")
+        Me.btnAddNew.UseVisualStyleBackColor = True
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'BaseForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1592, 941)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "BaseForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Student Information Application"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel6.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents txtTitle As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents lblPostCode As Label
+    Friend WithEvents txtPostCode As TextBox
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents lblPhone As Label
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnAddNew As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.Win32
+Imports System.Drawing.Text
+Imports Accessibility
+
+Public Class BaseForm
+
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private id As Integer
+
+    Private Sub LoadData()
+
+        'Create Command Object 
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT * FROM Students"
+        myCmd.ExecuteNonQuery()
+
+        Dim myDataAdapter As New SqlDataAdapter(myCmd)
+        Dim myDataTable As New DataTable
+        myDataAdapter.Fill(myDataTable)
+        DataGridView1.DataSource = myDataTable
+    End Sub
+
+    Private Sub Reset()
+        txtFirstName.Text = ""
+        txtLastName.Text = ""
+        txtAddress.Text = ""
+        txtPostCode.Text = ""
+        txtPhone.Text = ""
+    End Sub
+
+
+    Private Sub BaseForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        myConn = New SqlConnection("Initial Catalog=myconnector;" &
+            "Data Source=localhost;Integrated Security=SSPI;")
+
+        If myConn.State = ConnectionState.Open Then
+            myConn.Close()
+        End If
+
+        myConn.Open()
+        LoadData()
+    End Sub
+
+    Private Sub btnAddNew_Click(sender As Object, e As EventArgs) Handles btnAddNew.Click
+
+        myConn = New SqlConnection("Initial Catalog=myconnector;" &
+            "Data Source=localhost;Integrated Security=SSPI;")
+        myConn.Open()
+        myCmd = myConn.CreateCommand()
+        myCmd.CommandType = CommandType.Text
+        myCmd.CommandText =
+        "INSERT INTO Students (Firstname, Lastname, Address, PostCode, Phone)
+        VALUES('" & txtFirstName.Text & "', '" & txtLastName.Text _
+        & "', '" & txtAddress.Text & "', '" & txtPostCode.Text & "', '" & txtPhone.Text & "')"
+
+        myCmd.ExecuteNonQuery()
+        Reset()
+        LoadData()
+        MessageBox.Show("Record Sucessfully Inserted")
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+
+        Try
+            If myConn.State = ConnectionState.Open Then
+                myConn.Close()
+            End If
+
+            myConn.Open()
+
+            id = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+
+            myCmd = myConn.CreateCommand()
+            myCmd.CommandType = CommandType.Text
+            myCmd.CommandText = "SELECT * FROM Students where StudentID=" & id & ""
+            myCmd.ExecuteNonQuery()
+
+            Dim myDataAdapter As New SqlDataAdapter(myCmd)
+            Dim myDataTable As New DataTable
+            myDataAdapter.Fill(myDataTable)
+
+            Dim myDataReader As SqlClient.SqlDataReader
+            myDataReader = myCmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While myDataReader.Read
+
+                txtFirstName.Text = myDataReader.GetString(1).ToString()
+                txtLastName.Text = myDataReader.GetString(2).ToString()
+                txtAddress.Text = myDataReader.GetString(3).ToString()
+                txtPostCode.Text = myDataReader.GetString(4).ToString()
+                txtPhone.Text = myDataReader.GetString(5).ToString()
+
+            End While
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If myConn.State = ConnectionState.Open Then
+            myConn.Close()
+        End If
+
+        myConn.Open()
+
+        myCmd = myConn.CreateCommand()
+        myCmd.CommandType = CommandType.Text
+        myCmd.CommandText = "UPDATE Students SET Firstname='" + txtFirstName.Text +
+        "',Lastname='" + txtLastName.Text + "',Address='" +
+        txtAddress.Text + "',PostCode='" + txtPostCode.Text +
+        "', Phone='" + txtPhone.Text + "' WHERE StudentID=" & id & ""
+        myCmd.ExecuteNonQuery()
+
+        LoadData()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If myConn.State = ConnectionState.Open Then
+            myConn.Close()
+        End If
+
+        myConn.Open()
+
+        myCmd = myConn.CreateCommand()
+        myCmd.CommandType = CommandType.Text
+        myCmd.CommandText = "DELETE FROM Students WHERE StudentID=" & id & ""
+        myCmd.ExecuteNonQuery()
+
+        LoadData()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Reset()
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Dim iExit As DialogResult
+
+        iExit = MessageBox.Show("Confirm if you want to exit.",
+        "MySQL Connector", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If iExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
